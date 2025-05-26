d8e7ab6ed34ca13fd60ff7c3fa6ab6bc60ff2787
srsujon37 <srsujon97@gmail.com>
2025-05-26 12:23:17 +0600

    layout

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import NavBar from '../pages/Shared/NavBar';
+import Footer from '../pages/Shared/Footer';
+
+const RootLayout = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
